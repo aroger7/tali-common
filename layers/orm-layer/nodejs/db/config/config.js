34b115f435a82f556585de2350851267cc945d50
Adam Rogers <adam@adamrogers.ca>
2022-03-05 13:15:47 -0500

    update

--- layers/orm-layer/nodejs/db/config/config.js
+++ layers/orm-layer/nodejs/db/config/config.js
@@ -3,8 +3,9 @@ module.exports = {
     username: process.env.username,
     password: process.env.password,
     port: process.env.port,
-    host: process.env.host,
+    // host: process.env.host,
     // host: 'localhost',
+    host: 'tali-db-dev.cobq5f3c8nxj.us-east-1.rds.amazonaws.com',
     dialect: process.env.engine
   },
   local: {
